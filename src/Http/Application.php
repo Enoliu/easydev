7b57e76402c0f33d153db1af5e8773f6cd7a65ec
liuxiaolong <595929049@qq.com>
2021-05-22 10:25:11 +0800

    更新方法

--- src/Http/Application.php
+++ src/Http/Application.php
@@ -8,12 +8,12 @@ use Enoliu\EasyDev\Kernel\ServiceContainer;
  * Class Application
  *
  * @method array get(string $url, array $query = [])    // GET请求
- * @method array post(string $url, array $query = [])   // POST请求
- * @method array put(string $url, array $query = [])    // PUT请求
- * @method array delete(string $url, array $query = []) // DELETE请求
+ * @method array post(string $url, array $data = [])    // POST请求
+ * @method array put(string $url, array $data = [])     // PUT请求
+ * @method array delete(string $url, array $data = [])  // DELETE请求
  * @method array setHeader(string $key, mixed $value)   // 设置header
- * @method array setHeaders(array $headers = [])    // 设置多个header
- * @method array setTimeout(int $timeout = 10)  // 设置请求超时时间
+ * @method array setHeaders(array $headers = [])        // 设置多个header
+ * @method array setTimeout(int $timeout = 10)          // 设置请求超时时间
  */
 class Application extends ServiceContainer
 {
